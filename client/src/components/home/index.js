b03cee6c6db48b3cdb12a6cf58dc4ff3e4be6a83
CWoods <cwoods88keys@yahoo.com>
2023-02-18 18:53:49 -0600

    side drawer

---
+++ client/src/components/home/index.js
@@ -0,0 +1,8 @@
+const Home = () => {
+    return (
+        <>
+            Home alone
+        </>
+    )
+}
+export default Home
---
+++ client/src/components/navigation/header.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+import SideDrawer from './sideDrawer';
+
+const Header = () => {
+    return (
+        <nav className="navbar fixed-top">
+            <Link to="/" className='navbar-brand d-flex align-items-center fredoka_ff'>
+                Flickbase
+            </Link>
+            <SideDrawer/>
+        </nav>
+    )
+}
+export default Header;
